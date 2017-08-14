819428f9076b7aa334286ad20d27abbb324d856e
Sylphony <Sylphony@users.noreply.github.com>
2017-08-13 23:56:27 -0700

    Updated files

--- src/js/events.js
+++ src/js/events.js
@@ -1,12 +1,24 @@
 import Rx from "rxjs";
 
 const btnClick = document.getElementById("btn-click");
-
-console.log(btnClick);
+const input = document.getElementById("input");
 
 Rx.Observable
   // fromEvent(): Create an observable from an event
   .fromEvent(btnClick, "click")
-  .subscribe(() => {
+  .subscribe(event => {
     alert("Hello");
+  });
+
+Rx.Observable
+  // Like event listeners, it is possible to attach multiple events to an element
+  .fromEvent(btnClick, "click")
+  .subscribe(event => {
+    alert("Why");
+  });
+
+Rx.Observable
+  .fromEvent(input, "keyup")
+  .subscribe(event => {
+    console.log(event.target.value);
   });
